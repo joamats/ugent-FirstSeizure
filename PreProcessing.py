848c1fb8573ee5f5b0f2c09b2fb8f6b0c0598458
joamats <58477967+joamats@users.noreply.github.com>
2021-02-25 23:51:57 +0100

    Pre-Processing for all signals

--- PreProcessing.py
+++ PreProcessing.py
@@ -17,7 +17,7 @@ def import_eeg(filename):
                                       'T1', 'T2', 'CP1', 'CP2', 'CP5', 'CP6', 'FC1', 'FC2', 'FC5', 'FC6'])
     
     # Average referencing
-    raw.set_eeg_reference()
+    raw.set_eeg_reference(raw.ch_names[0:19])
     # Add ECG Channel
     raw.set_channel_types({'ECG': 'ecg'})
     # Set 10-20 montage
@@ -25,65 +25,53 @@ def import_eeg(filename):
     raw.set_montage(ten_twenty_montage)
     return raw
 
-#%% Load Data
+#%%
+def filter_eeg(raw):
+    f_stop = 50
+    f_low = 1
+    f_high = 100
+    
+    raw.notch_filter(f_stop)
+    raw.filter(f_low, f_high)
+    return raw
+
+#%% Load Filenames
 filenames = pd.read_excel('Metadata_train.xlsx')['Filename']
-f = filenames[0]
-raw = import_eeg(f)
+filenames = filenames.drop([101,231,232,233,234]) # wrong channels' names for some reason
 
-#%% Filter EEG
-f_stop = 50
-f_low = 1
-f_high = 100
-
-raw.notch_filter(f_stop)
-raw.filter(f_low, f_high)
-raw.plot_psd()
-print(raw.info)
-
-#%% ICA
-# ica = mne.preprocessing.ICA(n_components=7, random_state=43)
-# ica.fit(raw)
-# ica.plot_components()
-# ica.plot_sources(raw)
-
-#%% Blinks artifacts
-
+#%%
 raws = list()
 icas = list()
+idxs = list()
+li = 0
 
-for subj in range(1):
+for subj in filenames.index:
     # import signal
     raw = import_eeg(filenames[subj])
     # filter signal
-    raw.notch_filter(f_stop)
-    raw.filter(f_low, f_high)
-    raw.plot_psd()
+    raw = filter_eeg(raw)
     # fit ICA
-    ica = mne.preprocessing.ICA(n_components=7, random_state=43)
-    ica.fit(raw)
-    ica.plot_components()
-    if(subj<=1):
+    ica = mne.preprocessing.ICA(n_components=7, random_state=42)
+    ica.fit(raw, verbose=False)
+    
+    if(subj <= 1):
         icas.append(ica)
         raws.append(raw)
     else:
-        icas[1]=ica
-        raws[1]=raw
-    mne.preprocessing.corrmap(icas, template=(0, 0), threshold=0.9, label='blink')
-    mne.preprocessing.corrmap(icas, template=(0, 3), threshold=0.9, label='ecg')
+        icas[1] = ica
+        raws[1] = raw
+        
+    mne.preprocessing.corrmap(icas, template=(0, 0), threshold=0.8, label='blink', plot = False, verbose = False)
+    mne.preprocessing.corrmap(icas, template=(0, 3), threshold=0.9, label='ecg', plot = False, verbose = False)
 
-    icas[subj].plot_overlay(raws[subj], exclude=icas[subj].labels_['blink'],picks='eeg')
-    icas[subj].plot_overlay(raws[subj], exclude=icas[subj].labels_['ecg'],picks='eeg')
-
-#%% ECG artifacts
-# ica.plot_overlay(raw, exclude=[3], picks='eeg')
-
-#%% Exclude
-# ica.exclude = [0, 1, 3]
-
-#%% Plot
-orig_raw = raw.copy()
-ica.apply(raw)
-
-orig_raw.plot(duration=10, n_channels = 20, title="Original", remove_dc=False)
-raw.plot(duration=10, n_channels = 20, title="After ICA", remove_dc=False)
-
+    idx = [icas[li].labels_['blink'], icas[li].labels_['ecg']]
+    idxs.append(idx)
+    ica.exclude = [item for sublist in idx for item in sublist]
+    orig_raw = raw.copy()
+    ica.apply(raw, verbose=False)
+    
+    # orig_raw.plot(duration=10, n_channels = 20, title="Original", remove_dc = False)
+    # raw.plot(duration=10, n_channels = 20, title="After ICA", remove_dc = False)
+    
+    li=1
+    
