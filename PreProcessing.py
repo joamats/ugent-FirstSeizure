ee77dc80aaf0ef31c1e4256c39b3b5b00d96ab69
Guilherme Peralta <up201704277@fe.up.pt>
2021-03-02 09:28:06 +0100

    Random state definition

--- PreProcessing.py
+++ PreProcessing.py
@@ -95,7 +95,7 @@ def eeg_preprocessing(filename, icas, plot = False):
 def clean_epochs(epochs):
     ar = AutoReject(verbose=False)
     epochs_clean = ar.fit_transform(epochs)
-    reject = get_rejection_threshold(epochs)
+    reject = get_rejection_threshold(epochs, random_state=42)
     return epochs_clean, reject
 
     
