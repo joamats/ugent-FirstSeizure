2390a7634e78e77690302f34c1da42e470165edb
joamats <58477967+joamats@users.noreply.github.com>
2021-03-15 17:42:12 +0100

    Add Graph Measures from connectivity matrices

--- FeaturePreparation.py
+++ FeaturePreparation.py
@@ -4,10 +4,19 @@ import numpy as np
 
 #%% Auxiliary functions
 
+# retrieves all saved features in proper dict
+def _get_saved_features():
+    IMCOH = getPickleFile('../Features/' + 'imcoh')
+    PLV = getPickleFile('../Features/' + 'plv')
+    MI = getPickleFile('../Features/' + 'mi')
+    PDC = getPickleFile('../Features/' + 'pdc')
+    
+    return {'imcoh': IMCOH, 'plv': PLV, 'mi': MI, 'pdc': PDC}
+
+
 # transforms connectivity array into flat DataFrame
-def _triangular_to_1D(ft_arr, filename, bd_name, ms_type, ms_name):
-    ch_names = ['Fz', 'Cz', 'Pz', 'Fp1', 'F3', 'C3', 'P3', 'O1', 'F7', 'T3', 'T5', 
-                          'Fp2', 'F4', 'C4', 'P4', 'O2', 'F8', 'T4', 'T6']
+def _triangular_to_1D(ft_arr, ch_names, filename, bd_name, ms_type, ms_name):
+    
     bd_n = bd_name
     ms_t = ms_type[0]
     labels = []
@@ -19,47 +28,79 @@ def _triangular_to_1D(ft_arr, filename, bd_name, ms_type, ms_name):
     ft_df = pd.DataFrame(data=ft_flat, index=[filename], columns=labels)
     return ft_df.loc[:, (ft_df != 0).any(axis=0)]
 
+
+# def _graph_measures_to_1D(graph_ms, filename, bd_name, ms_name):
+    
+    
+
+#%% Produce Features Array for ML
+
+def make_features_array(fts):
+        
+    ch_names = ['Fz', 'Cz', 'Pz', 'Fp1', 'F3', 'C3', 'P3', 'O1', 'F7', 'T3', 'T5', 
+                          'Fp2', 'F4', 'C4', 'P4', 'O2', 'F8', 'T4', 'T6']
+    
+    filenames = pd.read_excel('Metadata_train.xlsx')['Filename']
+    ms_names = ['imcoh', 'plv', 'mi', 'pdc']
+    bd_names = ['Global', 'Delta', 'Theta', 'Alpha', 'Beta']
+    ms_types = ['Mean', 'Std']
+    
+    imcoh = {}
+    plv = {}
+    mi = {}
+    pdc = {}
+    
+    allFeatures = pd.DataFrame()
+    
+    for filename in filenames:
+        features_row = pd.DataFrame()
+        for ms_name in ms_names:
+            if ms_name == 'mi':
+                bd_names = ['Global']
+            else:
+                bd_names = ['Global', 'Delta', 'Theta', 'Alpha', 'Beta']
+                
+            for bd_name in bd_names:
+                for ms_type in ms_types:
+                    ft = fts[ms_name][filename][bd_name][ms_type]
+                    ft_df = _triangular_to_1D(ft, ch_names, filename, bd_name, ms_type, ms_name)
+                    features_row = pd.concat([features_row, ft_df], axis=1)
+                    
+        allFeatures = pd.concat([allFeatures, features_row], axis=0)
+    
+    return allFeatures
+
+
 #%% Run
 
-IMCOH = getPickleFile('../Features/' + 'imcoh')
-PLV = getPickleFile('../Features/' + 'plv')
-MI = getPickleFile('../Features/' + 'mi')
-PDC = getPickleFile('../Features/' + 'pdc')
+# fts = _get_saved_features()
+# allFeatures = make_features_array(fts)
+# createPickleFile(allFeatures, '../Features/' + 'allFeatures')
 
-fts = {'imcoh': IMCOH, 'plv': PLV, 'mi': MI, 'pdc': PDC}
+# filenames = pd.read_excel('Metadata_train.xlsx')['Filename']
+# f = filenames[[0]][0]
+# graph_ms = getPickleFile('../Features/' + 'graphMeasures')
 
-filenames = pd.read_excel('Metadata_train.xlsx')['Filename']
-ms_names = ['imcoh', 'plv', 'mi', 'pdc']
-bd_names = ['Global', 'Delta', 'Theta', 'Alpha', 'Beta']
-ms_types = ['Mean', 'Std']
+# ms_names = ['imcoh', 'plv', 'mi', 'pdc']
+# bd_names = ['Global', 'Delta', 'Theta', 'Alpha', 'Beta']
+# ms_types = ['betweness_centrality', 'clustering_coefficient',
+#             'global_efficiency', 'node_strengths']
+# str_types = ['incoming_flow', 'outgoing_flow']
 
-imcoh = {}
-plv = {}
-mi = {}
-pdc = {}
 
-allFeatures = pd.DataFrame()
-
-for filename in filenames:
-    features_row = pd.DataFrame()
-    for ms_name in ms_names:
-        if ms_name == 'mi':
-            bd_names = ['Global']
-        else:
-            bd_names = ['Global', 'Delta', 'Theta', 'Alpha', 'Beta']
-            
-        for bd_name in bd_names:
-            for ms_type in ms_types:
-                ft = fts[ms_name][filename][bd_name][ms_type]
-                ft_df = _triangular_to_1D(ft, filename, bd_name, ms_type, ms_name)
-                features_row = pd.concat([features_row, ft_df], axis=1)
+# for filename in filenames:
+#         features_row = pd.DataFrame()
+#         for ms_name in ms_names:
+#             if ms_name == 'mi':
+#                 bd_names = ['Global']
+#             else:
+#                 bd_names = ['Global', 'Delta', 'Theta', 'Alpha', 'Beta']
                 
-    allFeatures = pd.concat([allFeatures, features_row], axis=0)
-
-createPickleFile(allFeatures, '../Features/' + 'allFeatures')
-
-
-
-
-
+#             for bd_name in bd_names:
+#                 for ms_type in ms_types:
+#                     gr_ms = graph_ms[ms_name][filename][bd_name][ms_type]
+#                     ft_df = _triangular_to_1D(ft, ch_names, filename, bd_name, ms_type, ms_name)
+#                     features_row = pd.concat([features_row, ft_df], axis=1)
+                    
+#         allFeatures = pd.concat([allFeatures, features_row], axis=0)
 
---
+++ GraphMeasures.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import brainconn
+from FeaturePreparation import _get_saved_features
+from Pickle import createPickleFile
+
+#%% Computes Graph Measures from connectivity features
+
+def compute_graph_measures(fts):
+
+    filenames = pd.read_excel('Metadata_train.xlsx')['Filename']
+    ms_names = ['imcoh', 'plv', 'mi', 'pdc']
+    bd_names = ['Global', 'Delta', 'Theta', 'Alpha', 'Beta']
+    
+    # dict to store all measures from all subjects
+    graph_ms = {}
+    
+    for filename in filenames:
+        # dict to store each subject's measures
+        gr_ms_measure = {}
+        
+        for ms_name in ms_names:
+            
+            # MI does not have frequency bands
+            if ms_name == 'mi':
+                bd_names = ['Global']
+            else:
+                bd_names = ['Global', 'Delta', 'Theta', 'Alpha', 'Beta']
+            
+            # dict to store each band's measures
+            gr_ms_bds = {}   
+            for bd_name in bd_names:
+                
+                # get connectivity features
+                ft = fts[ms_name][filename][bd_name]['Mean']
+                
+                # pdc is directed, no need to get symmetric matrix
+                if ms_name != 'pdc':
+                    # transform triangular matrix to symmetric square
+                    ft = ft + ft.T - np.diag(np.diag(ft))
+                
+                # compute graphs measures
+                global_efficiency = brainconn.distance.efficiency_wei(ft)
+                betweenness_centr = brainconn.centrality.betweenness_wei(ft)
+                
+                # pdc is directed -> different functions
+                if ms_name == 'pdc':
+                    clustering_coef = brainconn.clustering.clustering_coef_wd(ft)
+                    in_strength, out_strength, _ = brainconn.degree.strengths_dir(ft)
+                    # save in dict
+                    gr_ms_bds[bd_name] = {
+                        'clustering_coefficient': clustering_coef,
+                        'global_efficiency': global_efficiency,
+                        'betweness_centrality': betweenness_centr,
+                        'incoming_flow': in_strength, 
+                        'outgoing_flow': out_strength }
+                else:
+                    clustering_coef = brainconn.clustering.clustering_coef_wu(ft)
+                    strengths = brainconn.degree.strengths_und(ft)
+                    # save in dict
+                    gr_ms_bds[bd_name] = {
+                        'clustering_coefficient': clustering_coef,
+                        'global_efficiency': global_efficiency,
+                        'betweness_centrality': betweenness_centr,
+                        'node_strengths': strengths }
+                    
+            gr_ms_measure[ms_name] = gr_ms_bds
+        graph_ms[filename] = gr_ms_measure
+        
+    return graph_ms
+
+#%% Run
+
+fts = _get_saved_features()
+graph_ms = compute_graph_measures(fts)
+createPickleFile(graph_ms, '../Features/' + 'graphMeasures')
+                
+                
